edc5691757f825f2630ca16c562edd070ed29fc2
nataliiacat <nataliagricaj6@gmail.com>
2024-11-29 22:17:04 +0200

    add signup and login forms in popup window

---
+++ main.js
@@ -0,0 +1,17 @@
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const loginSignupLink = document.querySelectorAll(".form-popup__box .bottom-link a")
+
+showPopupBtn.addEventListener("click", () => {
+  document.body.classList.toggle("show-popup");
+});
+
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+loginSignupLink.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup")
+  })
+})

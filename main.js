026387ff240d1e6917b9b5abe99eed61188e05b4
nataliiacat <nataliagricaj6@gmail.com>
2024-12-03 01:04:10 +0200

    Added smooth scroll to menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const navbarMenu = document.querySelector(".header__nav .header__menu");
 const menuBtn = document.querySelector(".menu-btn");
 const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const menuLinks = document.querySelectorAll(".header__menu a");
 const showPopupBtn = document.querySelector(".login-btn");
 const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
@@ -10,7 +11,31 @@ menuBtn.addEventListener("click", () => {
 navbarMenu.classList.toggle("show-menu");
 });
 
-hideMenuBtn.addEventListener("click", () => menuBtn.click());
+hideMenuBtn.addEventListener("click", () => {
+  navbarMenu.classList.remove("show-menu");
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navbarMenu.classList.remove("show-menu");
+  });
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      const targetPosition = targetElement.offsetTop; 
+
+      window.scrollTo({
+        top: targetPosition, 
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
 
 showPopupBtn.addEventListener("click", () => {
   document.body.classList.toggle("show-popup");
